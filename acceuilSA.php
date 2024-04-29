ee0106829c9853f1eaaab2fafa5ce82b2ea4944e
makrilena <161148888+makrilena@users.noreply.github.com>
2024-04-29 19:59:04 +0100

    Add files via upload

---
+++ acceuilSA.php
@@ -0,0 +1,10 @@
+<html>
+    <header>
+        <title>Acceuil SUPER Admin </title>
+    </header>
+    <body>
+    <a href="misson.php"><button>cree mission</button></a>
+    <a href="login.php"><button>cree compte</button></a>
+    <a href="consultation.php"><button>consutation</button></a>
+    </body>
+</html>
