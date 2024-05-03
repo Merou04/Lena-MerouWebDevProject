b20756adbd2978ad1513f34b43f61d7f4ecdc242
Raoufhmn <161158446+Raoufhmn@users.noreply.github.com>
2024-05-03 22:06:57 +0200

    Update Login.php

--- Login.php
+++ Login.php
@@ -3,59 +3,60 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Car Track</title>
+    <title>Login - CarTrack</title>
     <style>
         body {
-            background: linear-gradient(to right, orange, yellow, red); 
-
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
             display: flex;
             justify-content: center;
             align-items: center;
             height: 100vh;
-            margin: 0;
-            font-family: 'Arial', sans-serif;
-            color: #FFFFFF; /
+            background: linear-gradient(45deg, #172b4d, #2c3e50); 
+            color: #fff; 
         }
         form {
             width: 300px;
             padding: 30px;
-            background: white; 
+            background: rgba(255, 255, 255, 0.9);
             border-radius: 10px;
-            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2); 
+            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
         }
         h2 {
             text-align: center;
-            color: #333; 
-            margin-bottom: 20px; 
+            color: #333; /* Dark blue */
+            margin-bottom: 20px;
         }
         label {
-            display: block; 
-            margin-bottom: 5px; 
+            display: block;
+            margin-bottom: 5px;
+            color: #333; /* Dark blue */
         }
         input[type="text"], input[type="password"] {
-            width: calc(100% - 20px); 
+            width: calc(100% - 20px);
             padding: 10px;
-            margin-bottom: 15px; 
-            border: 1px solid #ccc; 
-            border-radius: 5px; 
+            margin-bottom: 15px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
         }
         input[type="submit"] {
             width: 100%;
-            background-color: #ff4500; 
-            color: white;
+            background-color: #ffdb58; 
+            color: #333; 
             padding: 10px;
             border: none;
             border-radius: 5px;
             cursor: pointer;
         }
         input[type="submit"]:hover {
-            background-color: #c83711; 
+            background-color: #ccab42; 
         }
     </style>
 </head>
 <body>
     <form action="/submit-your-login-form" method="post">
-        <h2>Login to Car Track</h2>
+        <h2>Login to CarTrack</h2>
         <div>
             <label for="userid">User ID:</label>
             <input type="text" id="userid" name="userid" required placeholder="Enter your User ID">
@@ -70,5 +71,3 @@
     </form>
 </body>
 </html>
-
-
