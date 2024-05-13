5e4f94cd617b3b3371167f44faad1fc7121d6057
Raoufhmn <161158446+Raoufhmn@users.noreply.github.com>
2024-05-14 00:13:55 +0200

    Update view drivers reports.php

--- view drivers reports.php
+++ view drivers reports.php
@@ -1,22 +1,22 @@
 <?php
-            // Exemple de code PHP pour récupérer des informations depuis la base de données
+           
             $host = "localhost";
             $dbuser = "root";
             $dbpass = "";
             $dbname = "CarTrack";
             $conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
 
-            // Vérifier la connexion
+            
             if (!$conn) {
                 die("Connection failed: " . mysqli_connect_error());
             }
 
-            // Exemple de requête SQL pour récupérer des informations
+           
             $sql = "SELECT * FROM reports";
             $result = mysqli_query($conn, $sql);
 
             if (mysqli_num_rows($result) > 0) {
-                // Afficher les données dans un tableau
+               
                 echo "<h3>Recent Reports</h3>";
                 echo "<table border='1'>";
                 echo "<tr><th>Subject</th><th>Message</th></tr>";
