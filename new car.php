03886111b9ca5259b1853380697929bd9ade0ca3
Merou04 <147769782+Merou04@users.noreply.github.com>
2024-05-13 18:00:32 +0100

    test

--- new car.php
+++ new car.php
@@ -64,48 +64,97 @@
         <h1>Add Vehicle</h1>
         <form action="process_form.php" method="POST">
             <div class="form-group">
-                <label for="name">Name of Vehicle:</label>
-                <input type="text" id="name" name="name" required>
+            <label for="name">Name of Vehicle:</label>
+            <input type="text" id="name" name="name" required>
             </div>
             <div class="form-group">
-                <label for="year">Year:</label>
-                <input type="number" id="year" name="year" min="1900" max="9999" required>
+            <label for="year">Year:</label>
+            <input type="number" id="year" name="year" min="1900" max="9999" required>
             </div>
             <div class="form-group">
-                <label for="licence_plate">License Plate:</label>
-                <input type="text" id="licence_plate" name="licence_plate" required>
+            <label for="licence_plate">License Plate:</label>
+            <input type="text" id="licence_plate" name="licence_plate" required>
             </div>
             <div class="form-group">
-                <label for="color">Color:</label>
-                <input type="text" id="color" name="color" required>
+            <label for="color">Color:</label>
+            <input type="text" id="color" name="color" required>
             </div>
             <div class="form-group">
-                <label for="type">Type:</label>
-                <select id="type" name="type" required>
-                    <option value="Sedan">Sedan</option>
-                    <option value="SUV">SUV</option>
-                    <option value="Truck">Truck</option>
-                    <option value="Van">Van</option>
-                    <option value="Other">Other</option>
-                </select>
+            <label for="type">Type:</label>
+            <select id="type" name="type" required>
+                <option value="Sedan">Sedan</option>
+                <option value="SUV">SUV</option>
+                <option value="Truck">Truck</option>
+                <option value="Van">Van</option>
+                <option value="Other">Other</option>
+            </select>
             </div>
             <div class="form-group">
-                <label for="last_mileage">Last Mileage Recorded:</label>
-                <input type="number" id="last_mileage" name="last_mileage" min="0" required>
+            <label for="last_mileage">Last Mileage Recorded:</label>
+            <input type="number" id="last_mileage" name="last_mileage" min="0" required>
             </div>
             <div class="form-group">
-                <label for="date_last_check">Date of Last Check:</label>
-                <input type="date" id="date_last_check" name="date_last_check" required>
+            <label for="date_last_check">Date of Last Check:</label>
+            <input type="date" id="date_last_check" name="date_last_check" required>
             </div>
             <div class="form-group">
-                <label for="mileage_next_maintenance">Mileage of Next Maintenance:</label>
-                <input type="number" id="mileage_next_maintenance" name="mileage_next_maintenance" min="0" required>
+            <label for="mileage_next_maintenance">Mileage of Next Maintenance:</label>
+            <input type="number" id="mileage_next_maintenance" name="mileage_next_maintenance" min="0" required>
             </div>
             <div class="form-group">
-                <label for="vehicle_code">Vehicle Code:</label>
-                <input type="text" id="vehicle_code" name="vehicle_code" required>
+            <label for="vehicle_code">Vehicle Code:</label>
+            <input type="text" id="vehicle_code" name="vehicle_code" required>
             </div>
             <button type="submit">Submit</button>
+        </form>
+
+        <?php
+        // Check if the form is submitted
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Retrieve the form data
+            $name = $_POST["name"];
+            $year = $_POST["year"];
+            $licence_plate = $_POST["licence_plate"];
+            $color = $_POST["color"];
+            $type = $_POST["type"];
+            $last_mileage = $_POST["last_mileage"];
+            $date_last_check = $_POST["date_last_check"];
+            $mileage_next_maintenance = $_POST["mileage_next_maintenance"];
+            $vehicle_code = $_POST["vehicle_code"];
+
+            // TODO: Insert the form data into the database
+            // You can use database functions like mysqli_query() or PDO to insert the data
+
+            // Redirect to a success page
+            header("Location: success.php");
+            exit;
+        }
+        ?>
+
+            <?php
+                        // Check if the form is submitted
+                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                            // Retrieve the form data
+                            $name = $_POST["name"];
+                            $year = $_POST["year"];
+                            $licence_plate = $_POST["licence_plate"];
+                            $color = $_POST["color"];
+                            $type = $_POST["type"];
+                            $last_mileage = $_POST["last_mileage"];
+                            $date_last_check = $_POST["date_last_check"];
+                            $mileage_next_maintenance = $_POST["mileage_next_maintenance"];
+                            $vehicle_code = $_POST["vehicle_code"];
+
+                            // TODO: Insert the form data into the database
+                            // You can use database functions like mysqli_query() or PDO to insert the data
+
+                            // Redirect to a success page
+                            header("Location: success.php");
+                            exit;
+                        }
+                    ?>
+
+
         </form>
     </div>
 </body>
