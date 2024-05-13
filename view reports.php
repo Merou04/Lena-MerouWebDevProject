854e5f2834ecb471e000d61cad58da42b06d83dd
Raoufhmn <161158446+Raoufhmn@users.noreply.github.com>
2024-05-14 00:26:57 +0200

    Update view reports.php

--- view reports.php
+++ view reports.php
@@ -1,4 +1,37 @@
+
 <?php
+           
+            $host = "localhost";
+            $dbuser = "root";
+            $dbpass = "";
+            $dbname = "CarTrack";
+            $conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+
+           
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+
+            // pour recuper les données de la bas de données
+            $sql = "SELECT * FROM reports";
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+                
+                echo "<h3>Recent Reports</h3>";
+                echo "<table border='1'>";
+                echo "<tr><th>Subject</th><th>Message</th></tr>";
+                while($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr><td>" . $row["subject"] . "</td><td>" . $row["message"] . "</td></tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "No reports found";
+            }
+
+            mysqli_close($conn);
+        ?>
+/* <?php
 session_start();
 
  $host = "localhost";
@@ -7,7 +40,7 @@ session_start();
  $dbname = "CarTrack";
  $conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
 
- ?>
+ ?>*/
 
 <!DOCTYPE html>
 <html lang="en">
